e719ae21b2867bdb264ab9a4c04509db2ab7c146
liujie <liujie@do365.com>
2020-08-04 00:23:03 +0800

    2020-08-03demo01

---
+++ app/src/main/java/com/jackliu/promote/controller/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.jackliu.promote.controller.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jackliu.promote.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/jackliu/promote/controller/fragment/MakeMoneyFragment.java
@@ -0,0 +1,38 @@
+package com.jackliu.promote.controller.fragment;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jackliu.promote.R;
+
+public class MakeMoneyFragment extends Fragment {
+
+    private MakeMoneyViewModel mViewModel;
+
+    public static MakeMoneyFragment newInstance() {
+        return new MakeMoneyFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.make_money_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MakeMoneyViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
