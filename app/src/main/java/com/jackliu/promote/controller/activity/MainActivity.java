ec3ed3546dad22017119980e8f866ca79d2e645d
yourname <youremail>
2020-08-04 17:59:49 +0800

    add Tablayout_ViewPager2

--- app/src/main/java/com/jackliu/promote/controller/activity/MainActivity.java
+++ app/src/main/java/com/jackliu/promote/controller/activity/MainActivity.java
@@ -1,9 +1,16 @@
 package com.jackliu.promote.controller.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.jackliu.promote.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomView = findViewById(R.id.bottom_nav);
+        NavController navController = Navigation.findNavController(this,R.id.fragment);
+        AppBarConfiguration configuration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this,navController,configuration);
+        NavigationUI.setupWithNavController(bottomView,navController);
     }
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/jackliu/promote/controller/fragment/MakeMoneyFragment.java
+++ app/src/main/java/com/jackliu/promote/controller/fragment/MakeMoneyFragment.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 
 import com.jackliu.promote.R;
 
@@ -26,6 +28,20 @@ public class MakeMoneyFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.make_money_fragment, container, false);
+
+        FragmentStateAdapter fragmentStateAdapter = new FragmentStateAdapter() {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                return null;
+            }
+
+            @Override
+            public int getItemCount() {
+                return 4;
+            }
+        }
+
     }
 
     @Override
