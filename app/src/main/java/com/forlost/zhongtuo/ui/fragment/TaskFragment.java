c30f534cbf7c456c5ded49e718513d33f4fcd88c
yourname <youremail>
2020-08-11 14:34:53 +0800

    我的页面添加卡组

--- app/src/main/java/com/forlost/zhongtuo/ui/fragment/TaskFragment.java
+++ app/src/main/java/com/forlost/zhongtuo/ui/fragment/TaskFragment.java
@@ -14,6 +14,7 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.forlost.zhongtuo.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -28,6 +29,9 @@ public class TaskFragment extends Fragment {
     private ViewPager viewpager;
     ArrayList fragmentList = new ArrayList<Fragment>();
     String[] temp = {"自由港", "众拓港"};
+
+    //悬浮按钮
+    private Button fab;
 
     public static TaskFragment newInstance() {
         return new TaskFragment();
